0bdfa00f3c3be8e1808645c97c71201068586085
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2023-07-16 19:40:58 +0900

    Initial Commit

---
+++ LaboVisionOS/LaboVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaboVisionOSApp.swift
+//  LaboVisionOS
+//
+//  Created by Ken Tsutsumi on 2023/07/16.
+//
+
+import SwiftUI
+
+@main
+struct LaboVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
